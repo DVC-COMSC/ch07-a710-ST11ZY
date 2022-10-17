4a5058009b8782af71e497e29ff2d941b54fd9eb
Your Name <you@example.com>
2022-10-17 13:42:58 -0700

    graph

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 data1 = [100, 90, 80, 60]
 data2 = [90, 80, 70, 100]
 labels = ['Math', 'English', 'Physics', 'Computer']
+x=np.arange(len(labels))
 names = ['Bill', 'Mary']
 
 fig, ax = plt.subplots()
-
+ax.bar(labels,data1, label='Bill')
+ax.bar(labels,data2, bottom=data1, label='Mary')
+ax.set_title("Stacked graph for scores")
+ax.legend()
 # ******************************
 # Make your code
 # ******************************
 
 
 fig.savefig('A10.png')
+plt.show()
